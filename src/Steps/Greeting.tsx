c7d8752e3d23a0a8aae22186715e72d9077f0b20
Jiewen Zheng <jiewenz@microsoft.com>
2023-09-21 10:41:12 -0700

    more

--- src/Steps/Greeting.tsx
+++ src/Steps/Greeting.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 export const Greeting = (props: {
   yourName: string,
   setResult: (stepName: string) => void
 }) => {
+  const [bodyResult, setbodyResult] = useState<string>("");
+  useEffect(() => {
+    let mainAsyncFunction =async ()=> {
+    const result = await fetch("http://jzlinux.westus2.cloudapp.azure.com:8080/getnumber");
+    const json = await result.json();
+    setbodyResult(JSON.stringify(json, null, 2));
+    }
+    mainAsyncFunction();
+  },[]);
+
   return (
     <div>
       <div>
         Hello {props.yourName}
+        Results from a Rest API call:
+        <pre>{bodyResult || null}</pre>
       </div>
       <div>
-      <button name="click me to changed primary state" onClick={() => props.setResult("greeting")}>click me</button>
+        <button type="button" onClick={() => props.setResult("greeting")}>click me</button>
       </div>
     </div>
   );
 };
+
